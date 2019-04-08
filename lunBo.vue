ec6ca79705b24b2867152526626b0a0719aa1528
398847134 <49388528+398847134@users.noreply.github.com>
2019-04-08 16:41:40 +0800

    Update lunBo.vue

--- lunBo.vue
+++ lunBo.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="main">
       <h1>测试上传</h1>
+      <transition-group name='image' tag="ul" class="swiper_img_ul">
+        <li v-for="(pic,index) in pics" :key="pic" v-show="index==showpic" @touchstart="stop" @touchend="start">
+          <img :src="pic" @touchmove="change" @touchstart="getposition" @touchend="turn">
+        </li>
+      </transition-group>
   </div>
 </template>
 
@@ -9,8 +14,45 @@ export default {
   name: 'lunBo',
   data () {
     return {
-
+      pics: [], // 轮播图片
+      showpic: 0, // 控制图片是否显示
+      positionX: 0 // 鼠标点击的坐标
     }
+  },
+  methods: {
+    stop () {
+      clearInterval(this.timer)
+    },
+    start () {
+      clearInterval(this.timer)
+      this.timer = setInterval(this.changeIndex, 5000)
+    },
+    getposition (e) {
+      this.positionX = e.changedTouches[0].clientX
+    },
+    turn (e) {
+      let ta = this.moveposition - this.positionX
+      if (ta > 0) {
+        if (ta > window.screen.width / 2) {
+          this.goto(this.showpic - 1)
+        }
+      } else {
+        if (-ta > window.screen.width / 2) {
+          this.goto(this.showpic + 1)
+        }
+      }
+    },
+    change (e) {
+      this.moveposition = e.changedTouches[0].clientX
+    },
+    changeIndex () {
+      if (this.showpic >= this.pics.length - 1) {
+        this.showpic = 0
+      } else {
+        this.showpic++
+      }
+    }
+
   }
 }
 </script>

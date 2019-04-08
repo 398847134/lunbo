2a204c422ee461872c6afcc9781f021c4aae2706
398847134 <49388528+398847134@users.noreply.github.com>
2019-04-08 15:45:05 +0800

    Create lunBo.vue

---
+++ lunBo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+      <h1>测试上传</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lunBo',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
